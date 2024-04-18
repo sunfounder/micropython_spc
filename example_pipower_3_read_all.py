df6d385f926b9efb0e43016de7ee1d2917ee2bc0
Cavon Lee <cavonxx@gmail.com>
2024-04-18 16:09:33 +0800

    format

--- example_pipower_3_read_all.py
+++ example_pipower_3_read_all.py
@@ -20,7 +20,7 @@ def main():
         print(f"Output voltage: {data_buffer['output_voltage']} mV")
         print(f"Battery voltage: {data_buffer['battery_voltage']} mV")
         print(f"Battery percentage: {data_buffer['battery_percentage']} %")
-        print(f"Power source: {data_buffer['power_source']} ({'Battery' if data_buffer['power_source'] == spc.BATTERY else 'External'})")
+        print(f"Power source: {data_buffer['power_source']} - {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'External'}")
         print(f"Input plugged in: {data_buffer['is_input_plugged_in']}")
         print(f"Battery plugged in: {data_buffer['is_battery_plugged_in']}")
         print(f"Charging: {data_buffer['is_charging']}")
@@ -36,7 +36,7 @@ def main():
             shutdown_request_str = 'Button'
         else:
             shutdown_request_str = 'Unknown'
-        print(f"Shutdown request: {data_buffer['shutdown_request']}({shutdown_request_str})")
+        print(f"Shutdown request: {data_buffer['shutdown_request']} - {shutdown_request_str}")
         print(f'Board id: {spc.read_board_id()}')
         print(f"Always on: {spc.read_always_on()}")
         print(f"Power source voltage: {spc.read_power_source_voltage()} mV")
--- example_pipower_3_read_individual.py
+++ example_pipower_3_read_individual.py
@@ -18,7 +18,7 @@ def main():
         print(f"Battery voltage: {spc.read_battery_voltage()} mV")
         print(f"Battery percentage: {spc.read_battery_percentage()} %")
         power_source = spc.read_power_source()
-        print(f"Power source: {power_source} ({'Battery' if power_source == spc.BATTERY else 'External'})")
+        print(f"Power source: {power_source} - {'Battery' if power_source == spc.BATTERY else 'External'}")
         print(f"Input plugged in: {spc.read_is_input_plugged_in()}")
         print(f"Battery plugged in: {spc.read_is_battery_plugged_in()}")
         print(f"Charging: {spc.read_is_charging()}")
@@ -35,7 +35,7 @@ def main():
             shutdown_request_str = 'Button'
         else:
             shutdown_request_str = 'Unknown'
-        print(f"Shutdown request: {shutdown_request}({shutdown_request_str})")
+        print(f"Shutdown request: {shutdown_request} - {shutdown_request_str}")
         print(f'Board id: {spc.read_board_id()}')
         print(f"Always on: {spc.read_always_on()}")
         print(f"Power source voltage: {spc.read_power_source_voltage()} mV")
